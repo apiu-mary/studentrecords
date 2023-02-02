dc8b45e94037008a0f96591c68d5b02bfd7e91ec
apiuuagou@gmail.com <apiuagou@gmail.com>
2023-02-02 08:45:05 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var fullName = "maryapiu"
+    var age = 34
+    var phoneNumber = 363555
+    var weightInkgs = 45
+    var isKenyan = "false"
+    var isnotKenyan ="true"
+
+
+}
